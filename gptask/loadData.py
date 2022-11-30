ee827d840b1fc50828d7750a6a830bfaf24d0135
jvmatosjr <jvmatos.jr@gmail.com>
2022-11-30 16:03:37 +0100

    test docker

--- gptask/loadData.py
+++ gptask/loadData.py
@@ -225,7 +225,8 @@ def initTables():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    initTables()
-    loadPlaces(placesPath)
-    loadPeople(peoplePath)
+    #initTables()
+    #loadPlaces(placesPath)
+    #loadPeople(peoplePath)
+    print("test docker")
 
